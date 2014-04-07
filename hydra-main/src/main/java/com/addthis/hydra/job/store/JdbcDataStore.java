351938fe5eafbb520701ecefaf954ec632ee86d7
al <al@addthis.com>
2014-04-07 10:12:54 -0400

    Implemented parent/child methods for JdbcDataStore.

--- hydra-main/src/main/java/com/addthis/hydra/job/store/JdbcDataStore.java
+++ hydra-main/src/main/java/com/addthis/hydra/job/store/JdbcDataStore.java
@@ -1,5 +1,6 @@
 package com.addthis.hydra.job.store;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,23 +13,29 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.addthis.basis.util.Parameter;
-import com.addthis.basis.util.Strings;
 
 import com.addthis.codec.Codec;
+import com.addthis.codec.CodecExceptionLineNumber;
+import com.addthis.codec.CodecJSON;
+import com.addthis.maljson.JSONException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JdbcDataStore implements SpawnDataStore {
+    private static Logger log = LoggerFactory.getLogger(JdbcDataStore.class);
+    private static final CodecJSON codecJSON = new CodecJSON();
     private static final String description = "jdbc_v1";
     private final Connection conn;
-    private static final String pathKey = "p";
-    private static final String valueKey = "v";
-    private static final String dbName = "SpawnData1";
-    private static final String tableName = "jdbc_datastore_v2";
-    private static final String queryTemplate = "select ? from " + tableName;
-    private static final String insertTemplate = "insert into " + tableName + "(" + pathKey + "," + valueKey + ") values( ? , ? ) " +
-                                                 "on duplicate key update " + valueKey + "=values(" + valueKey + ")";
+    private static final String pathKey = "path";
+    private static final String valueKey = "val";
+    private static final String parentKey = "parent";
+    private static final String childKey = "child";
+    private final String tableName = "jdbc_datastore_v4";
     private static final int maxPathLength = Parameter.intValue("jdbc.datastore.max.path.length", 200);
+    private static final String parentChildSeparator = "|||";
 
-    public JdbcDataStore() throws Exception {
+    public JdbcDataStore(String dbName) throws Exception {
         Class.forName("org.drizzle.jdbc.DrizzleDriver");
         Properties props = new Properties();
         props.put("user", "spawn");
@@ -36,12 +43,14 @@ public class JdbcDataStore implements SpawnDataStore {
         conn = DriverManager.getConnection("jdbc:mysql:thin://localhost:3306/" + dbName, props);
         conn.setAutoCommit(false);
         createStartupCommand().execute();
-        // Create database/table
-        // Test put/get
     }
 
     private PreparedStatement createStartupCommand() throws SQLException {
-        return conn.prepareStatement("CREATE TABLE IF NOT EXISTS " + tableName + "( " + pathKey + " VARCHAR(" + maxPathLength + ") NOT NULL, PRIMARY KEY(" + pathKey + "), " + valueKey + " TEXT)");
+        return conn.prepareStatement("CREATE TABLE IF NOT EXISTS " + tableName + "( "
+                                     + pathKey + " VARCHAR(" + maxPathLength + ") NOT NULL, PRIMARY KEY(" + pathKey + "), "
+                                     + valueKey + " TEXT, "
+                                     + parentKey + " TEXT, "
+                                     + childKey + " TEXT )");
 
     }
 
@@ -57,6 +66,7 @@ public class JdbcDataStore implements SpawnDataStore {
     }
 
     private PreparedStatement makeInsertStatement() throws SQLException {
+        String insertTemplate = "insert into " + tableName + "(" + pathKey + "," + valueKey + "," + parentKey + "," + childKey + ") values( ? , ? , ? , ? ) on duplicate key update " + valueKey + "=values(" + valueKey + ")";
         return conn.prepareStatement(insertTemplate);
     }
 
@@ -80,14 +90,113 @@ public class JdbcDataStore implements SpawnDataStore {
 
     @Override
     public Map<String, String> get(String[] paths) {
-        try {
-            // If path contains a comma, need to encode
-            PreparedStatement preparedStatement = makeQueryStatement(pathKey + "," + valueKey, pathKey, Strings.join(paths, ","));
-            ResultSet resultSet = preparedStatement.executeQuery();
-            Map<String, String> rv = new HashMap<>();
-            for (int i=0; i<resultSet.getFetchSize(); i++) {
-                rv.put(resultSet.getString(0), resultSet.getString(1));
+        // Should be possible to get multiple values from a single query.
+        if (paths == null) {
+            return null;
+        }
+        Map<String, String> rv = new HashMap<>();
+        for (String path : paths) {
+            String val = get(path);
+            if (val != null) {
+                rv.put(path, val);
             }
+        }
+        return rv;
+    }
+
+    private void insert(String path, String value, String parent, String childId) throws SQLException {
+        if (path.length() > maxPathLength) {
+            throw new IllegalArgumentException("Input path longer than max of " + maxPathLength);
+        }
+        PreparedStatement preparedStatement = makeInsertStatement();
+        preparedStatement.setString(1, path);
+        preparedStatement.setString(2, value);
+        preparedStatement.setString(3, parent);
+        preparedStatement.setString(4, childId);
+        preparedStatement.execute();
+        conn.commit();
+    }
+
+    @Override
+    public void put(String path, String value) throws Exception {
+        insert(path, value, null, null);
+    }
+
+    private static String makeChildKey(String parent, String childId) {
+        return parent + parentChildSeparator + childId;
+    }
+
+    @Override
+    public void putAsChild(String parent, String childId, String value) throws Exception {
+        insert(makeChildKey(parent, childId), value, parent, childId);
+    }
+
+    @Override
+    public <T extends Codec.Codable> boolean loadCodable(String path, T shell) {
+        String val = get(path);
+        if (val == null) {
+            return false;
+        }
+        try {
+            codecJSON.decode(shell, path.getBytes());
+            return true;
+        } catch (CodecExceptionLineNumber codecExceptionLineNumber) {
+            return false;
+        } catch (JSONException e) {
+            return false;
+        }
+
+    }
+
+    @Override
+    public String getChild(String parent, String childId) throws Exception {
+        return get(makeChildKey(parent, childId));
+    }
+
+    @Override
+    public void deleteChild(String parent, String childId) {
+        delete(makeChildKey(parent, childId));
+    }
+
+    @Override
+    public void delete(String path) {
+        try {
+            String deleteTemplate = "delete from " + tableName + " where " + pathKey + "= ?";
+            PreparedStatement preparedStatement = conn.prepareStatement(deleteTemplate);
+            preparedStatement.setString(1, path);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String makeChildQueryTemplate(boolean includeChildValues) {
+        return "select " + childKey + (includeChildValues ? "," + valueKey : "") + " from " + tableName + " where " + parentKey + "= ?";
+    }
+
+    private ResultSet getResultsForQuery(String template, String path) throws SQLException {
+        PreparedStatement preparedStatement = conn.prepareStatement(template);
+        preparedStatement.setString(1, path);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        boolean foundRows = resultSet.next();
+        if (!foundRows) {
+            return null;
+        }
+        return resultSet;
+    }
+
+    @Override
+    public List<String> getChildrenNames(String path) {
+        String template = makeChildQueryTemplate(false);
+        try {
+            ResultSet resultSet = getResultsForQuery(template, path);
+            if (resultSet == null) {
+                return null;
+            }
+            ArrayList<String> rv = new ArrayList<>();
+            do {
+                rv.add(resultSet.getString(1));
+            } while (resultSet.next());
             return rv;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -95,55 +204,30 @@ public class JdbcDataStore implements SpawnDataStore {
     }
 
     @Override
-    public void put(String path, String value) throws Exception {
-        if (path.length() > maxPathLength) {
-            throw new IllegalArgumentException("Input path longer than max of " + maxPathLength);
-        }
-        // Update if exists already
-        PreparedStatement preparedStatement = makeInsertStatement();
-        preparedStatement.setString(1, path);
-        preparedStatement.setString(2, value);
-        preparedStatement.execute();
-        conn.commit();
-    }
-
-    @Override
-    public void putAsChild(String parent, String childId, String value) throws Exception {
-
-    }
-
-    @Override
-    public <T extends Codec.Codable> boolean loadCodable(String path, T shell) {
-        return false;
-    }
-
-    @Override
-    public String getChild(String parent, String childId) throws Exception {
-        return null;
-    }
-
-    @Override
-    public void deleteChild(String parent, String childId) {
-
-    }
-
-    @Override
-    public void delete(String path) {
-
-    }
-
-    @Override
-    public List<String> getChildrenNames(String path) {
-        return null;
-    }
-
-    @Override
     public Map<String, String> getAllChildren(String path) {
-        return null;
+        String template = makeChildQueryTemplate(true);
+        try {
+            ResultSet resultSet = getResultsForQuery(template, path);
+            if (resultSet == null) {
+                return null;
+            }
+            HashMap<String, String> rv = new HashMap<>();
+            do {
+                rv.put(resultSet.getString(1), resultSet.getString(2));
+            } while (resultSet.next());
+            return rv;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public void close() {
-
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            log.warn("Failed to close sql connection", e);
+        }
     }
 }
--- hydra-main/src/test/java/com/addthis/hydra/job/JdbcDataStoreTest.java
+++ hydra-main/src/test/java/com/addthis/hydra/job/JdbcDataStoreTest.java
@@ -1,8 +1,17 @@
 package com.addthis.hydra.job;
 
+import java.util.List;
+import java.util.Map;
+
 import com.addthis.hydra.job.store.JdbcDataStore;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class JdbcDataStoreTest {
 
@@ -10,12 +19,27 @@ public class JdbcDataStoreTest {
     public void test() throws Exception {
 
         JdbcDataStore jdbcDataStore = new JdbcDataStore();
-        jdbcDataStore.put("key6", "value6");
-        jdbcDataStore.put("key7", "value7!!!!'',',;';;'");
-        System.out.println(jdbcDataStore.get("key5"));
-        System.out.println(jdbcDataStore.get("key6"));
-        System.out.println(jdbcDataStore.get("key7"));
-        // put, get, etc
+        String key1 = "key1";
+        String val1 = "value1";
+        String key2 = "key2";
+        String val2 = "value!!\"{ !!'' }[,'],;';;'\n_\\";
+
+        jdbcDataStore.put(key1, "old");
+        jdbcDataStore.put(key1, val1);
+        jdbcDataStore.put(key2, val2);
+        assertNull("should get null for non-inserted key", jdbcDataStore.get("key5"));
+        assertEquals("should get latest value", val1, jdbcDataStore.get(key1));
+        assertEquals("should correctly fetch value with extra characters", val2, jdbcDataStore.get(key2));
+        Map<String, String> expected = ImmutableMap.of(key1, val1, key2, val2);
+        assertEquals("should get expected map from multi-fetch call", expected, jdbcDataStore.get(new String[]{key1, key2, "otherKey"}));
+        jdbcDataStore.putAsChild("parent", "child1", val1);
+        jdbcDataStore.putAsChild("parent", "child2", "val2");
+        jdbcDataStore.deleteChild("parent", "child2");
+        jdbcDataStore.putAsChild("parent", "child3", val2);
+        List<String> expectedChildren = ImmutableList.of("child1", "child3");
+        assertEquals("should get expected children list", expectedChildren, jdbcDataStore.getChildrenNames("parent"));
+        Map<String, String> expectedChildrenMap = ImmutableMap.of("child1", val1, "child3", val2);
+        assertEquals("should get expected children map", expectedChildrenMap, jdbcDataStore.getAllChildren("parent"));
         jdbcDataStore.close();
     }
 
